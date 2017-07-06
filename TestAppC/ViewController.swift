63048dedb7159e5a55f3b29da45fb3871aab65a5
Reuben Lim <reubenlim@Reubens-MacBook-Air.local>
2017-07-06 15:56:01 +0800

    Initial Commit

---
+++ TestAppC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppC
+//
+//  Created by Reuben Lim on 06/07/2017.
+//  Copyright © 2017 Reuben Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
